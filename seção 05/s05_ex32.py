7cec9d1ed5151b9d60ca6e354e05d63c22e905a8
ANDREOGP33 <andregasparoni690@gmail.com>
2023-05-28 18:57:40 -0300

    seção 5
    Signed-off-by: ANDREOGP33 <andregasparoni690@gmail.com>

--- seção 05/s05_ex32.py
+++ seção 05/s05_ex32.py
@@ -1,17 +1,17 @@
-preco_antigo = float(input("Digite o valor antigo do produto : "))
+preco_antigo = float(input("Digite o valor antigo do produto: "))
 
 if preco_antigo <= 50:
     preco_novo = preco_antigo + (preco_antigo * 0.05)
-elif preco_antigo > 50 and preco_antigo <= 100:
+elif 50 < preco_antigo <= 100:
     preco_novo = preco_antigo + (preco_antigo * 0.10)
-elif preco_antigo > 101:
+else:
     preco_novo = preco_antigo + (preco_antigo * 0.15)
 
 if preco_novo < 80:
-        print("Barato!")
-elif preco_novo >= 80 and preco_novo < 120:
-        print("Normal!")
-elif preco_novo > 120 and preco_novo <= 200:
-        print("Caro!")
-elif preco_novo > 201:
-        print("Muito caro!")
+    print("Barato!")
+elif 80 <= preco_novo < 120:
+    print("Normal!")
+elif 120 <= preco_novo <= 200:
+    print("Caro!")
+else:
+    print("Muito caro!")
