1fd100bdf515c2a5796a2ee1b8561b2d3eb0be16
ANDREOGP33 <andregasparoni690@gmail.com>
2023-06-01 14:03:12 -0300

    seção 5

---
+++ seção 05/s05_ex36.py
@@ -0,0 +1,17 @@
+valor_venda = float(input("Digite o valor de sua venda: "))
+
+if valor_venda >= 100000:
+    print(f"Comissão: {700 + ((0.16 * valor_venda) + valor_venda)}")
+elif 100000 > valor_venda >= 80000:
+    print(f"Comissão: {650 + ((0.14 * valor_venda) + valor_venda)}")
+elif 80000 > valor_venda >= 60000:
+    print(f"Comissão: {600 + ((0.14* valor_venda) + valor_venda)}")
+elif 60000 > valor_venda >= 40000:
+    print(f"Comissão: {550 + ((0.14 * valor_venda) + valor_venda)}")
+elif 40000 > valor_venda >= 20000:
+    print(f"Comissão: {500 + ((0.14 * valor_venda) + valor_venda)}")
+elif 20000 > valor_venda:
+    print(f"Comissão: {400 + ((0.14* valor_venda) + valor_venda)}")
+
+
+     
