48b6cfabb6a5aa7acb3aeed1bdccaa2f8cf73fb3
ANDREOGP33 <andregasparoni690@gmail.com>
2023-06-03 15:26:54 -0300

    seção 5
    Signed-off-by: ANDREOGP33 <andregasparoni690@gmail.com>

--- seção 05/s05_ex38.py
+++ seção 05/s05_ex38.py
@@ -4,9 +4,16 @@ dia = int(input("Dia: "))
 mes = int(input("Mês: "))
 ano = int(input("Ano: "))
 
-if 0 <= dia >31:
-    print("Data invalida!")
-elif mes == 2 and dia > 28:
+if 0 >= dia > 31:
     print("Data invalida!")
 elif ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
-    
+    if mes == 30 or mes == 31:
+        print("Data invalida!")
+    else:
+        print("Data valida")
+elif mes == 2 and dia >= 29:
+    print("Data invalida!")
+elif 0 >= mes > 12:
+    print("Data invalida!")
+else:
+    print("Data val8ida")
