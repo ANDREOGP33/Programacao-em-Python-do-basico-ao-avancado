b6b0c6ce7010740ba9cb337db07029c7cfa6b52d
ANDREOGP33 <andregasparoni690@gmail.com>
2023-06-09 07:27:10 -0300

    seção 6

--- seção 06/s06_ex07.py
+++ seção 06/s06_ex07.py
@@ -1,1 +1,11 @@
-qtd = 
+total = 0
+
+for i in range (0, 10):
+    print("Digite um numero inteiro: ")
+    numero = int(input())
+    if numero > 0:
+        total = total + numero
+
+    media = total / 10
+
+print(f"Media: {media}")
