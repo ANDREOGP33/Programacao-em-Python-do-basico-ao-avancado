09d6148ee811f52e4388e2670c6484fac524950a
ANDREOGP33 <andregasparoni690@gmail.com>
2023-06-16 14:46:30 -0300

    seção 6

---
+++ seção 06/s06_ex35.py
@@ -0,0 +1,12 @@
+inicio = int(input("Digite um valor inicial: "))
+fim = int(input("Digite um valor final: "))
+intervalo = range(inicio,fim)
+
+soma = 0
+if fim < inicio:
+    print("Intervalos de valores invalido!")
+else:
+    for i in intervalo:
+        if i % 2 != 0:
+            soma += i
+print(soma)
