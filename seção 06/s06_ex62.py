49af2c33aca4681b5de0566094d03a259383f1ac
ANDREOGP33 <andregasparoni690@gmail.com>
2023-06-29 07:51:16 -0300

    seção 6

--- seção 06/s06_ex62.py
+++ seção 06/s06_ex62.py
@@ -2,16 +2,33 @@ unidades = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "o
 dezenas = ["", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
 centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
 
-soma = 0
 lista = range(1, 1001)
 caracteres = []
 
 for i in lista:
     if i < 10:
         caracteres.append(unidades[i])
-    if i < 100:
-        caracteres.append(dezenas[i])
-    if i > 100:    
-        caracteres.append(centenas[i])
+    elif i < 100:
+        dezena, unidade = divmod(i, 10)
+        if unidade == 0:
+            caracteres.append(dezenas[dezena])
+        else:
+            caracteres.append(dezenas[dezena] + " e " + unidades[unidade])
+    else:
+        centena, resto = divmod(i, 100)
+        if centena <= 9:
+            if resto == 0:
+                caracteres.append(centenas[centena])
+            else:
+                dezena, unidade = divmod(resto, 10)
+                if unidade == 0:
+                    caracteres.append(centenas[centena] + " e " + dezenas[dezena])
+                else:
+                    caracteres.append(centenas[centena] + " e " + dezenas[dezena] + " e " + unidades[unidade])
 
-print(caracteres)
+quantidade_caracteres = 0
+
+for palavra in caracteres:
+    quantidade_caracteres += len(palavra)
+
+print("Quantidade de caracteres:", quantidade_caracteres)
