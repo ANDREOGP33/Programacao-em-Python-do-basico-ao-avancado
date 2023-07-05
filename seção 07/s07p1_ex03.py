dbc7fdea214f673900b9246fef25ee004ae9f1a4
ANDREOGP33 <andregasparoni690@gmail.com>
2023-07-05 13:55:17 -0300

    seção 7

---
+++ seção 07/s07p1_ex03.py
@@ -0,0 +1,8 @@
+numeros = [0.453, -1.237, 2.876, -0.689, 1.934, -3.512, 0.781, -2.019, 1.245, -0.976]
+resultado = []
+
+for i in numeros:
+    valor_qad = i ** 2
+    resultado.append(valor_qad)
+
+print(resultado)
