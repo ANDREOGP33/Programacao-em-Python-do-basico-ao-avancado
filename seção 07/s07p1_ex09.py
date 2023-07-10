3a6d5bd5bcccfa2c0a52afd90b7c7f3c15a40add
ANDREOGP33 <andregasparoni690@gmail.com>
2023-07-10 19:34:50 -0300

    seção 7
    Signed-off-by: ANDREOGP33 <andregasparoni690@gmail.com>

---
+++ seção 07/s07p1_ex09.py
@@ -0,0 +1,14 @@
+vetor = []
+
+for i in range(0, 6):
+    valor = int(input("Digite um valor par: "))
+    if valor % 2 != 0:
+        while True:
+            valor = int(input("Digite um valor par: "))
+            if valor % 2 == 0:
+                vetor.append(valor)
+            break
+    else:
+        vetor.append(valor)
+
+print(vetor)
